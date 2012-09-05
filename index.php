e91cd9026a949af1fe90a3fd14e6e38a9611ebea
Pepe Doval <pepellou@gmail.com>
2012-09-05 19:06:39 +0200

    Get photos from flickr

--- index.php
+++ index.php
@@ -1,6 +1,53 @@
 <?php
 	require_once dirname(__FILE__)."/mobile-detection.php";
 
+	function getPhotos(
+		$user,
+		$tag
+	) {
+		$selected_photos = array();
+		$user = "27418660@N05"; // xavierbelho
+		$user = "63176109@N03"; // pepellou
+		$tag = "acampadaobradoiro";
+		$xml = file_get_contents(
+			"http://api.flickr.com/services/feeds/photos_public.gne?id={$user}"
+		);
+		$xml = preg_replace('/(<|<\/)([a-z0-9]+):/i','$1$2_',$xml);
+		$photos = simplexml_load_string($xml);
+		foreach ($photos->entry as $photo) {
+			$theLink = "";
+			foreach ($photo->link as $link) {
+				$attributes = $link->attributes();
+				if ($attributes->rel == "alternate") {
+					$theLink = $attributes->href;
+				}
+			}
+			foreach ($photo->category as $category) {
+				$attributes = $category->attributes();
+				if ($attributes["scheme"] = "http://www.flickr.com/photos/tags/") {
+					if ($attributes["term"] == $tag) {
+						$public_page = file_get_contents($theLink);
+						$lat = strpos($public_page, '"perm_viewgeo":"0","geo"') + 35;
+						$lat = strpos($public_page, ":", $lat) + 1;
+						$end_lat = strpos($public_page, ",", $lat);
+						$lng = strpos($public_page, ":", $end_lat) + 1;
+						$end_lng = strpos($public_page, ",", $lng);
+						$theLatitude = substr($public_page, $lat, $end_lat - $lat);
+						$theLongitude = substr($public_page, $lng, $end_lng - $lng);
+
+						$selected_photos []= array(
+							"title" => $photo->title,
+							"link" => $theLink,
+							"lat" => $theLatitude,
+							"lng" => $theLongitude
+						);
+					}
+				}
+			}
+		}
+		return $selected_photos;
+	}
+
 	function getVideos(
 		$user,
 		$tag
@@ -36,7 +83,6 @@
 		return $selected_videos;
 	}
 
-	$videos = getVideos("pepellou", "music");
 ?>
 <!DOCTYPE html>
 <html>
@@ -218,21 +264,31 @@
 			</p>
 		</div>
 	</div>
+
 	<div id="videos">
-<?php
-
-	foreach ($videos as $video) { ?>
-		<div class="video">
-			<div name="title"><?php echo $video['title']; ?></div>
-			<div name="link"><?php echo $video['link']; ?></div>
-			<div name="gps">
-				<div name="lat"><?php echo $video['lat']; ?></div>
-				<div name="lng"><?php echo $video['lng']; ?></div>
+	<?php 	foreach (getVideos("pepellou", "music") as $video) { ?>
+			<div class="video">
+				<div name="title"><?php echo $video['title']; ?></div>
+				<div name="link"><?php echo $video['link']; ?></div>
+				<div name="gps">
+					<div name="lat"><?php echo $video['lat']; ?></div>
+					<div name="lng"><?php echo $video['lng']; ?></div>
+				</div>
 			</div>
-		</div>
-	<?php }
+	<?php 	} ?>
+	</div>
 
-?>
+	<div id="photos">
+	<?php 	foreach (getPhotos("pepellou", "music") as $photo) { ?>
+			<div class="photo">
+				<div name="title"><?php echo $photo['title']; ?></div>
+				<div name="link"><?php echo $photo['link']; ?></div>
+				<div name="gps">
+					<div name="lat"><?php echo $photo['lat']; ?></div>
+					<div name="lng"><?php echo $photo['lng']; ?></div>
+				</div>
+			</div>
+	<?php 	} ?>
 	</div>
 </body>
 </html>
