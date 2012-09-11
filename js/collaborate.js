558df3e0b0bc7f93fe4747fd6bfd8b44c8f007b3
Dani Lago <dlagosuarez@gmail.com>
2012-09-11 19:10:04 +0200

    delete collaborate.js file

--- js/collaborate.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$("#collaborate_button").mouseup(function() {
-		doModal(function() {
-			$("#contact").css("display","inline");
-			$("#contact").css("left", (parseInt($("body").css("width")) / 2) - (parseInt($("#contact").css("width")) / 2));
-		});
-	});
-});
