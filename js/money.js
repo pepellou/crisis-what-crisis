97c2b7a3e8a2fe6d1c9b37a1ddaf530a6d090614
Dani Lago <dlagosuarez@gmail.com>
2012-09-11 23:29:37 +0200

    extract money.css and money.js

---
+++ js/money.js
@@ -0,0 +1,13 @@
+$(function() {
+	var money = new Array(
+		[2500, '€ Estimated', '#2c4390'],
+		[100,  '€ Collected so far', '#666']
+	);
+	$('#money').jqBarGraph({
+		data: money,
+		colors: ['#2c4390'] ,
+		showValuesColor: '#2c4390',
+		width: 320,
+		height: 280
+	});
+});
