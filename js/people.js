1032155ab8f8d93eabb7e4519af43b1c633407a7
Dani Lago <dlagosuarez@gmail.com>
2012-09-14 17:16:12 +0200

    change label 'web' by 'contact' in people

--- js/people.js
+++ js/people.js
@@ -61,7 +61,7 @@ function showPeople(
 		$("#people .why .text").html(people.why);
 		$("#people .how .text").html(people.how);
 		$("#people .web").html("web: <a href='" + people.web + "' target='blank'>" + people.web + "</a>");
-		$("#people .contact").html("web: <a href='mailto:" + people.contact + "'>" + people.contact + "</a>");
+		$("#people .contact").html("contact: <a href='mailto:" + people.contact + "'>" + people.contact + "</a>");
 		$("#people .photo").css("background-image","url(" + people.photo + ")");
 		for (i in people.links) {
 			$("#people .links").append("<a href='" + people.links[i] + "' target='blank'><div class='link'>" + people.links[i] + "</div></a>");
