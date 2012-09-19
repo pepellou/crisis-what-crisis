4fe2418843c44c3864f8a4c9e40fd68b178754a1
Dani Lago <dlagosuarez@gmail.com>
2012-09-19 20:13:19 +0200

    readjust postion of motorbike in money

--- js/money.js
+++ js/money.js
@@ -3,6 +3,6 @@ function calculateMoney() {
 	var width = $('.km').width();
 	var widthMoto = $('.motorbike img').width();
 
-	$('.km_yellow').css("width", money * (width/3000) + "px");
-	$('.motorbike img').css("left", (money * ((width/3000)))-(widthMoto) + "px");
+	$('.km_yellow').css("width", money * (width/3080) + "px");
+	$('.motorbike img').css("left", (money * ((width/3080)))-(widthMoto) + "px");
 }
