62e206fdceaa0a122fb7b8c03085e7b921eadea7
Pepe Doval <pepellou@gmail.com>
2012-10-19 11:06:08 +0200

    Donate in mobile

--- mobile.php
+++ mobile.php
@@ -24,4 +24,26 @@
 	<a href="http://www.youtube.com/watch?v=ln3tGnzGT9k" target="_blank">
 		<img id="play_video" src="play_video.png" />
 	</a>
+	<div id="donate">
+		<form class="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_donations">
+			<input type="hidden" name="business" value="project@crisis-whatcrisis.com">
+			<input type="hidden" name="currency_code" value="EUR">
+			<input type="hidden" name="lc" value="ES">
+			<input type="hidden" name="item_name" value="Crisis What Crisis">
+			<input type="hidden" name="no_note" value="0">
+			<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+			<input type="hidden" name="return" value="https://twitter.com/intent/tweet?source=webclient&text=I%20support%20Crisis%20What%20Crisis%20@crisisWcrisis%20%23crisisWhatcrisis%20http://www.crisis-whatcrisis.com">
+			<input type="hidden" name="cancel_return" value="http://www.crisis-whatcrisis.com">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal. The fast and secure way to pay online.">
+			<img alt="" border="0" src="https://www.paypalobjects.com/es_ES/i/scr/pixel.gif" width="1" height="1">
+		</form>
+		<div style="margin-bottom:15px">
+			<p style="text-align:center;margin:0px;">Or transfer to:</p>
+			<p style="text-align:center;margin:0px;font-weight:bold">2080-0395-60-3040002377</p>
+			<p style="text-align:center;margin:0px;">IBAN - ES3320800395603040002377</p>
+			<p style="text-align:center;margin:0px;">Swift/BIC - CAGLESMMXXX</p>
+			<p style="text-align:center;margin:0px;">Novagalicia Banco</p>
+		</div>
+	</div>
 </body>
