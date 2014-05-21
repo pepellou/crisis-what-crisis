8914429c058c4cd4d4867463d1b71f8d9524e219
Eru <eanovoa@gmail.com>
2014-05-21 20:18:29 +0200

    Add html hidden text for mobile

--- mobile.php
+++ mobile.php
@@ -24,4 +24,79 @@
 	<a href="http://www.youtube.com/watch?v=ln3tGnzGT9k" target="_blank">
 		<img id="play_video" src="play_video.png" />
 	</a>
+    <div style="position: absolute; z-index: -1; color: #aaa;">
+        <h1>Crisis what crisis: An optimistic answer</h2>
+        <h2>what</h2>
+        <p>
+            It's a documentary portraying the 2012's Europe.
+        </p>
+        <p>
+            A trip from Galicia, in the Iberian Peninsula's Northwest extreme, 
+            to the Pireo in Greece, across Portugal, Spain and Italy (PIGS)*.
+            It's a critical eye to the CRISIS.
+        </p>
+        <p>
+            A practical and brave answer to that questions we should ask:<br/>
+            What is the crisis? <br/>
+            Where does it come from?<br/>
+            Where does it lead us? <br/>
+            And now what?
+        </p>
+        <h2>why</h2> <p>
+            The crisis that was a deceleration and now seems an hecatomb
+            had to last two years and it'll make six. We don't see the light
+            at the end of the tunnel, and if we saw it we would think that
+            it's the train coming to finish us.
+        </p>
+        <p>
+            The degree of collective pessimism gripping us is more desperate
+            and immobilizer than financial imponderables. We have fear, and as
+            british sociologist Frank Furedi said long before the crisis arose,
+            ubiquitous and persistent fear generates doom and resignation.
+        </p>
+        <p>
+            We need to visualize alternative answers, other possible futures,
+            because if we don't imagine them, we will never be able to make
+            them happen.
+        </p>
+        <h2>who</h2> <p>
+            Graphical and audiovisual creator Xavier Belho leads the motorbike 
+            and the camera.
+        </p>
+        <p>
+            Accompanying him from Galicia, Irmáns Karapatrov and the collective N&oacute;Com&uacute;n.
+        </p>
+        <p>
+            And along the way he will be meeting the main characters of this story:
+            PIGS inhabitants that refuse to see themselves reflected in the 
+            hopelessness mirror and face the crisis with courage and geat ideas.
+        </p>
+        <h2>where</h2>
+        <p>
+            There where there is an intelligent answer to the crisis.
+        </p>
+        <p>
+            For now, the route will pass close to these cities:
+        </p>
+        <p>
+            <strong>Santiago de Compostela - Vigo - Oporto - Viseu - Salamanca - Madrid - Toledo - 
+            Valencia - Tarragona - Barcelona - Roma - Napoles - Bari - Brindisi - Patras - Atenas</strong>
+        </p>
+        <h2>how</h2>
+        <p>
+            The project is planned in two different stages:
+            <ol>
+                <li>
+                    The main characters will tell their stories, where they are developing their projects,
+                    directly to the camera (Canon 7D), through little videobooth style interviews.
+                    These microinterviews will be uploaded to the Web and Social networks, thus building an
+                    interactive map of the trip, so the audience will be able to interact almost on realtime.
+                </li>
+                <li>
+                    At the end of the trip, a narrative documentary will be built complementing
+                    selected statements of the main characters with contextual information and images.
+                </li>
+            </ol>
+        </p>
+    </div>
 </body>
