bed8b692f5b785790e7f409ac61a9e1b1a3ce69e
Pepe Doval <pepellou@gmail.com>
2014-06-02 19:11:36 +0200

    Dynamic sitemap

---
+++ sitemap.php
@@ -0,0 +1,17 @@
+<?php
+	header("Content-type: text/xml");
+
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+   <url>
+      <loc>http://crisis-whatcrisis.com/</loc>
+      <lastmod><?php echo date('Y-m-d'); ?></lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1</priority>
+   </url>
+
+</urlset>
+
