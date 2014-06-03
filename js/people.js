52e9d12d1b653d56a7fca0a28fb98185e3478ab4
Pepe Doval <pepellou@gmail.com>
2014-06-03 10:05:52 +0200

    Some almost real people data

--- js/people.js
+++ js/people.js
@@ -1,24 +1,47 @@
 people = [
 	{
 		"type": "people",
-		"who": "Fulanito de Mengano",
-	   	"where": "En un lugar de la mancha",
-	   	"what": "Molinos de viento solares",
-	   	"why": "Por que hay sol y viento y es gratis",
-	   	"how" : "???",
-	   	"web" : "http://www.web1.com",
-	   	"contact" : "email@email.com",
+		"who": "Fátima García Doval",
+	   	"where": "Santiago de Compostela",
+	   	"what": "Accegal",
+	   	"why": "Mucha gente pequeña, en lugares pequeños, haciendo cosas pequeñas, puede cambiar el mundo.",
+	   	"how" : "En la actualidad el trabajo está centrado en la comunicación, especialmente alternativa y aumentativa. Trabajamos conjuntamente con otros centros, asociaciones y particulares, de modo totalmente desinteresado y sin ánimo de lucro para que nuestras aplicaciones sean gratuitas y funcionen en dispositivos de bajo costo.",
+	   	"web" : "http://www.accegal.org/",
+	   	"contact" : "contacto@accegal.org",
 	   	"links" : [
-	   		"link1",
-	   		"link2"
+	   		"https://es-es.facebook.com/Accegal",
+	   		"http://www.youtube.com/user/accegal",
+			"https://twitter.com/Accegal"
 	   	],
-	   	"photo" : "img/people/who.png",
+	   	"photo" : "img/people/Fatima.png",
 	   	"images" : [
-	   		"http://www.crisis-whatcrisis.com/img/people/image1.png",
-	   		"http://www.crisis-whatcrisis.com/img/people/image2.png"
+	   		"img/people/Accegal1.png",
+	   		"img/people/Accegal2.png"
 	   	],
-   		"lat" : "42.340212",
-   		"lng" : "-7.869221"
+		"lat" : "42.884974",
+		"lng" : "-8.556708"
+	},
+	{
+		"type": "people",
+		"who": "Xavi Gost",
+	   	"where": "Valencia",
+	   	"what": "La Cova de Benimaclet",
+	   	"why": "Hazlo tú mismo, rechaza los dogmas y desconfía de las modas",
+	   	"how" : "En La Cueva de Benimaclet hay programadores haciendo software, pero también cursos de magia, talleres para aprender a radiar ruedas de bicicleta, cajas de verdura ecológica, cine, mercaditos de trueque, cursos de formación, talleres de desobediencia civil...",
+	   	"web" : "https://www.facebook.com/lacuevadebenimaclet",
+	   	"contact" : "lacovadebenimaclet@gmail.com",
+	   	"links" : [
+	   		"https://trello.com/b/wYQZxmvz/la-cueva",
+	   		"https://www.google.com/calendar/embed?src=6u8npmhel4qp5djn6bvl140hvk%40group.calendar.google.com&ctz=Europe%2FMadrid",
+			"https://twitter.com/XLaCovaX"
+	   	],
+	   	"photo" : "img/people/XaviGost.png",
+	   	"images" : [
+	   		"img/people/LaCova1.png",
+	   		"img/people/LaCova2.png"
+	   	],
+   		"lat" : "39.488443",
+   		"lng" : "-0.360331"
 	},
 ]
 
