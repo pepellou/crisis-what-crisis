56946c17aef371129adc3b821693556e3ddc346b
Pepe Doval <pepellou@gmail.com>
2014-07-15 15:46:08 +0200

    Extract roadmap js

--- roadmap.php
+++ roadmap.php
@@ -24,97 +24,9 @@
 		src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD8VKWnsMR8-zmp5dW7YOInsVjib26h840&sensor=false">
 	</script>
 		
+    <script language="Javascript" src="js/roadmap.js"></script>
+
 	<!-- Developed by Pepe Doval - see http://www.crisis-whatcrisis.com/humans.txt -->
-
-	<script language="javascript">
-        $(function() {
-            var speechBubbles = [];
-
-            $('#milestones .milestone').each(function() {
-                speechBubbles.push({
-                    id: $(this).find('[name="id"]').text(),
-                    start: $(this).find('[name="start"]').text(),
-                    start: parseInt($(this).css('margin-left')),
-                    milestone: $(this).find('[class="signalBot"]').text(),
-                    link: $(this).find('[name="link"]').text(),
-                    text: $(this).find('[name="text"]').text()
-                });
-            });
-
-            var lastPosition = 0;
-            var opacity = 0;
-            var lastBubble = null;
-
-            function scrollEventHandler(position) {
-                var bikePosition = position + 400;
-                var currentBubble = null;
-                var offset = 0;
-                speechBubbles.forEach(function(bubble) {
-                    if (bikePosition >= bubble.start && bikePosition <= bubble.start + 800) {
-                        currentBubble = bubble;
-                    }
-                });
-                if ((position != lastPosition) && (currentBubble != null)) {
-                    offset = bikePosition - currentBubble.start;
-                    if ((0 <= offset) && (offset <= 200)) {
-                        opacity = offset/200;
-                    }
-                    if ((600 <= offset) && (offset <= 800)) {
-                        opacity = 1 - (offset-600)/200;
-                    }
-                    $('.background_front').css('opacity',opacity);
-                }
-                lastPosition = position;
-                if (currentBubble != lastBubble) {
-                    if (currentBubble == null) {
-                        $('#speechBubble').css('display', 'none');
-                        $('.background_front').css('opacity','0');
-                    } else {
-                        $('#speechBubble div h1').html(currentBubble.milestone);
-                        $('#speechBubble div a').attr('href', currentBubble.link);
-                        $('#speechBubble span').html(currentBubble.text);
-                        $('#speechBubble').css('display', 'block');
-                        $img = $('<img/>').attr('src', 'img/roadmap/roadmap_'+(currentBubble.id)+'.jpg').load(function() {
-                            $('.background_front').attr('src','img/roadmap/roadmap_'+(currentBubble.id)+'.jpg');
-                            $(this).remove();
-                            $img = null;
-                        });
-                    }
-                    lastBubble = currentBubble;
-                }
-            }
-
-            setTimeout(function() {
-                scrollEventHandler($(window).scrollLeft());
-                setTimeout(arguments.callee, 20);
-            }, 20);
-
-            function displayWheel(e) {
-                var evt = window.event || e;
-                var delta = evt.detail? evt.detail*(-40) : evt.wheelDelta;
-                $(window).scrollLeft($(window).scrollLeft() - delta/3);
-            }
-
-            var mouseWheelEvt = (/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel";
-     
-            if (document.attachEvent)
-                document.attachEvent("on" + mouseWheelEvt, displayWheel);
-            else if (document.addEventListener)
-                document.addEventListener(mouseWheelEvt, displayWheel, false);
-
-            $(window).bind('keydown keypress', function(e) {
-                if ((e.keyCode == 37)||(e.keyCode == 38)) {
-                    e.preventDefault();
-                    $(window).scrollLeft($(window).scrollLeft() - 40);
-                }
-                if ((e.keyCode == 39)||(e.keyCode == 40)) {
-                    e.preventDefault();
-                    $(window).scrollLeft($(window).scrollLeft() + 40);
-                }
-            });
-            $('#road').focus();
-        });
-    </script>
 </head>
 <body>
     <div class="background">
