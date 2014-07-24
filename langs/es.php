17fa597faf14ab557367fd2d69f8b4138dd67ecd
Eru <eanovoa@gmail.com>
2014-07-24 10:10:56 +0200

    Add new spanish texts

--- langs/es.php
+++ langs/es.php
@@ -9,50 +9,48 @@
     define (META_LANGUAGE, 'es, español');
     define (META_DESCRIPTION, 'Un documental que retrata la Europa de 2012. Un viaje en moto que parte de Galicia, en el extremo Noroeste de la Pen&iacute;nsula ib&eacute;rica y llega hasta el Pireo, en Grecia, pasando por Portugal, Espa&ntilde;a e Italia (PIGS)*. Una mirada cr&iacute;tica a la CRISIS. Una respuesta pr&aacute;ctica y valiente a las preguntas que si no nos hacemos deber&iacute;amos hacernos: Qu&eacute; es la crisis? De d&oacute;onde viene? A d&oacute;nde nos lleva? Y ahora, qu&eacute;?');
     define (WHAT, '<p>
-				Un documental que retrata la Europa de 2012.
+Crisis What Crisis es un viaje en moto a través de los países protagonistas de la *CRISIS* (PIGS.). Queremos hacer visibles a personas y proyectos que están dando una respuesta optimista e innovadora. Crisis What Crisis: an optimistic answer es una mirada constructiva hacia la crisis. Una respuesta práctica y valiente sobre nuevas formas de organización y producción.
 			</p>
 			<p>
-				Un viaje en moto que parte de Galicia, en el extremo Noroeste de la Pen&iacute;nsula ib&eacute;rica 
-				y llega hasta el Pireo, en Grecia, pasando por Portugal, Espa&ntilde;a e Italia (PIGS)*.
-				Una mirada cr&iacute;tica a la CRISIS.
-			</p>
-			<p>
-				Una respuesta pr&aacute;ctica y valiente a las preguntas que si no nos hacemos deber&iacute;amos hacernos:<br/>
-			</p>
-			<ul>
-				<li>Qu&eacute; es la crisis? </li>
-				<li>De d&oacute;nde viene?</li>
-				<li>A d&oacute;nde nos lleva? </li>
-				<li>Y ahora, qu&eacute;?</li>
-			</ul>
-			<p>
-                * PORTUGAL, ITALY, GREECE, SPAIN ...
+El nuestro es un viaje de búsqueda compartida a través de colectivos y personas que disfrutan más de la experiencia que del dinero. Rastrearemos organizaciones que promuevan el conocimiento abierto, el autoconsumo y la cooperación basándose en un modelo de negocio sostenible a largo plazo. La felicidad no es lo que nos habían contado, es lo que nos estamos contando.
 			</p>');
     define (WHY, '<p>
-				La crisis que fue desaceleraci&oacute;n y ahora parece hecatombe ten&iacute;a que durar dos a&ntilde;os
-				y ya va para seis. No le vemos la luz al final del t&uacute;nel y, si la vi&eacute;ramos,
-				pensar&iacute;amos que es el tren que nos viene a rematar.
+Vivimos en el Sur de Europa, en unos países que tienen una tradición autoritaria de la que hemos salido hace poco, con unas clases políticas sometidas al poder económico y que nada han hecho para mejorar el bienestar de sus pueblos. Han utilizado las políticas para generar negocio rápido apoyados por la banca y los poderes económicos. 
 			</p>
 			<p>
-				El grado de pesimismo colectivo que nos atenaza es m&aacute;s desesperante e
-				immovilizador que los imponderables financieros. Tenemos miedo y, como dijo el
-				soci&oacute;logo brit&aacute;nico Frank Furedi bastante antes de que surgiese la
-				crisis, el miedo ubicuo, persistente, genera fatalidad y resignaci&oacute;n.
+La crisis ha sido creada por la especulación económica global por parte de sectores financieros y políticos. No existe por tanto una crisis causada por los pueblos y su falta de espíritu trabajador o innovador. Los PIGS no somos los pueblos que sufrimos esos movimientos especulativos de los poderes económicos y políticos, los PIGS somos pueblos que han sufrido regímenes autoritarios, guerras civiles, represión y violencia, y aún así somos pueblos creativos, innovadores y sociales.
 			</p>
 			<p>
-				Necesitamos visualizar otras respuestas, otros futuros posibles, porque
-				si no los imaginamos nunca seremos capaces de hacerlos realidad.
+“El cerdo doméstico adulto tiene un cuerpo pesado y redondeado, hocico comparativamente largo y flexible, patas cortas con pezuñas (cuatro dedos) y una cola corta. La piel, gruesa pero sensible, está cubierta en parte de ásperas cerdas y exhibe una amplia variedad de colores y dibujos. A pesar de su apariencia son animales ágiles, rápidos e inteligentes.”
+			</p>
+			<p>
+Aquí nace CWC para demostrar que existe un amplio movimiento en la base que hace temblar la torre del sistema, esto entre comillas, con nuevas ideas y modos de relacionarse q se basan en la sostenibilidad, solidaridad, cooperación y la libertad. 
+			</p>
+			<p>
+Necesitamos visualizar otras respuestas, otros futuros posibles, porque si no los
+imaginamos nunca seremos quién de hacerlos realidad.
+			</p>
+			<p>
+En febrero de 2013 nace mi hija Helena . Este documental es también un regalo para ella, y toda esta generación, para que cuando pueda verlo con capacidad de análisis se fije en el mundo y el contexto en el que nació, esperando que le sirva de algún modo para enfrentarse a su vida.
 			</p>');
     define (WHO, '<p>
-				El creador gr&aacute;fico y audiovisual Xavier Belho dirige la moto y la c&aacute;mara. 
+Mi nombre es Xavier Belho y me dedico al diseño gráfico y a la realización
+audiovisual. A los 23 años me fuí de casa de mis padres. Desde ese instante empecé a realizar todo tipo de actividades para poder comer. Vendí jamones, di clases de refuerzo a niños de primaria, gerencié un pub y hasta acabé siendo Director de una Sucursal.  Incluso trabajé como diseñador gráfico para una productora audiovisual y monté una productora con tres socios más. Todas estas actividades tuvieron éxito y fracaso… pero ninguna me llenó… hasta ahora. Por lo tanto, he caido y me he levantado siempre con una idea en la cabeza y pocos apoyos. Al fin y al cabo soy un optimista…. ¿hay alguien ahí? La respuesta espero encontrarla en CWC.
 			</p>
 			<p>
-				Le acomp&aacute;&ntilde;an, desde Galicia, los Hermanos Karapatrov y el colectivo N&oacute;Com&uacute;n.
+Si consigo con vuestra ayuda iniciar este proyecto haré un viaje
+en moto, una Yamaha Virago 1100 del año 97 de tercera mano, para rodar un
+documental que me llevará desde el Finisterre gallego hasta las puertas de Occidente, a
+través de los países protagonistas de la CRISIS (PIGS).
+CWC es un documental que retrata la Europa de 2014. Un viaje en moto en
+solitario que parte de Galicia, en el extremo noroeste de la Península Ibérica y llega
+hasta el Pireo (Atenas), en Grecia, pasando por Portugal, España e Italia (PIGS)*.
 			</p>
 			<p>
-				Y por el camino ir&aacute; conociendo a los protagonistas de esta historia:
-				habitantes de los PIGS que se niegan a verse reflejados en el espejo de la 
-				desesperaci&oacute;n y que afrontan la crisis con valent&iacute;a y buenas ideas.
+Contaré con el apoyo de los ExHermanos Karapatrov ( algunos de ellos colaboran de forma incondicional en el proyecto)  y el colectivo NóComún ( nos ayudaron en los primeros pasos y nos ayudarán en la difusión) , Casa das
+Crechas ( ayuda económica y de contactos), Asociación Cidade Vella de Compostela ( difundir el proyecto), los Dirty Socks (Son un grupo de jovenes que hacen música en inglés y con los q colaboré con algo de diseño) con la banda sonora, NARF (músico silvestre con él que me fuí a Londres a hacer una sesión de fotos, a Mozambique a grabar un vídeo, y con el q camino por la vida con optimismo, me ayudará en lo q precise)
+entre otros colectivos y personas que manifestarán su soporte públicamente en el
+momento de iniciar la aventura.
 			</p>');
     define (WHERE, '<p>
 				All&iacute; donde haya una respuesta inteligente e innovadora a la crisis.
@@ -65,6 +63,18 @@
 				Valencia - Tarragona - Barcelona - Roma - Napoles - Bari - Brindisi - Patras - Atenas</strong>
 			</p>');
     define (HOW, '<p>
+Desde niño me han gustado los libros de viajes, desde Julio Verne a Joseph Conrad y particularmente el libro de Ted Simon, Los viajes de Jupiter, que narra su vuelta al mundo en moto y su vivencia a lo largo del viaje. Mi afición por las motos nace del cine y de una película en partícular, Easy Rider, dónde la moto y el viaje son uno.
+			</p>
+			<p>
+Ahora es mi turno y realizar un viaje en mi vieja moto, cruzando Europa por el sur cómo lo hizo en el  siglo IV la viajera Egeria, y descubrir ese nuevo mundo que se está construyendo día día con optimismo para tener un futuro que se rija por valores verdaderamente humanos.
+			</p>
+			<p>
+Viajando en moto no puedo permitirme llevar un voluminoso equipo: contaré
+con una pequeña cámara de vídeo y fotos que grabará en fullHD; un miniequipo de
+sonido, un portátil para ir editando algunas imágenes y subirlas a la red a medida que
+voy avanzando.
+			</p>
+			<p>
 				El proyecto se plantea como una construcci&oacute;n modular con dos etapas diferenciadas:
 				<ol>
 					<li>
