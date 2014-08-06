d6b8c0cc22b205dc0b2d6930f10bc9bf3e0fb424
Eru <eanovoa@gmail.com>
2014-08-06 14:34:14 +0200

    Add initial rute points to home map

--- js/map/trip.js
+++ js/map/trip.js
@@ -1,50 +1,92 @@
 var trip = [
 	{   
-		name: "Start Point",
+		name: "Santiago de Compostela",
 		lat: 42.849084,
 		lng: -8.574164,
 		type: 'trip'
 	},  
 	{   
-		name: "Fake Stop #1",
-		lat: 42.3,
-		lng: -8.2,
+		name: "Oporto",
+		lat: 41.167952,
+		lng: -8.6959013,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #2",
-		lat: 41.9,
-		lng: -7.8,
+		name: "Lisboa",
+		lat: 38.7222322,
+		lng: -9.2081752,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #3",
-		lat: 41.3,
-		lng: -7.3,
+		name: "Badajoz",
+		lat: 38.8794293,
+		lng: -7.0394921,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #4",
-		lat: 41.1,
-		lng: -6.7,
+		name: "Caceres",
+		lat: 39.4752562,
+		lng: -6.4412633,
+		type: 'trip'
+	},  
+	{   
+		name: "Madrid",
+		lat: 40.416755,
+		lng: -3.7726288,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #5",
-		lat: 40.7,
-		lng: -6.0,
+		name: "Valencia",
+		lat: 39.4698872,
+		lng: -0.4451267,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #6",
-		lat: 40.3,
-		lng: -5.5,
+		name: "Barcelona",
+		lat: 41.39479,
+		lng: 2.1487679,
 		type: 'trip'
 	},
 	{   
-		name: "Fake Stop #8",
-		lat: 40,
-		lng: -5,
+		name: "Roma",
+		lat: 41.8723683,
+		lng: 12.4113417,
 		type: 'trip'
 	},
+	{   
+		name: "Brindisi",
+		lat: 40.6327074,
+		lng: 17.872923,
+		type: 'trip'
+	},
+	{   
+		name: "Patras",
+		lat: 38.2466194,
+		lng: 21.6657354,
+		type: 'trip'
+	},
+	{   
+		name: "Atenas",
+		lat: 37.9838969,
+		lng: 23.6605213,
+		type: 'trip'
+	},
+	{   
+		name: "Thessalonika",
+		lat: 40.6400425,
+		lng: 22.8755805,
+		type: 'trip'
+	},  
+	{   
+		name: "Pireas",
+		lat: 37.9451692,
+		lng: 23.5813827,
+		type: 'trip'
+	},  
+	{   
+		name: "Pireas",
+		lat: 37.9451692,
+		lng: 23.5813827,
+		type: 'trip'
+	},  
 ];
