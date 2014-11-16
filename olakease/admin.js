53a59598233c003cb52f3fcf38b0050d7df4389f
Pepe Doval <pepe.doval@getsetpay.com>
2014-11-16 18:35:19 +0100

    Admin: youtube videos list

--- olakease/admin.js
+++ olakease/admin.js
@@ -3,11 +3,36 @@ var DB = new Firebase("https://crisis-whatcrisis.firebaseio.com");
 var videos = DB.child('videos');
 
 var errors = function(error) {
-	console.log("The read failed: " + error.code);
+	console.log("Error: " + error.code);
+};
+
+var Videos = { web: [], youtube: [] };
+
+var Paint = {
+	list: function(where, list) {
+		$(where).html('');
+		list.forEach(function(item) {
+			$(where).append(ich.item_tpl(item));
+		});
+	}
 };
 
 $(function() {
 	videos.on("child_added", function(video) {
-		$('#videos').append(ich.video_tpl(video.val()));
+		Videos.web.push(video.val());
+		Paint.list('#videos_web', Videos.web);
 	}, errors);
 });
+
+var loadYoutube = function(data) {
+	var entries = data.feed.entry || [];
+	$(function() {
+		for (var i = 0; i < entries.length; i++) {
+			Videos.youtube.push({
+				title: entries[i].title.$t,
+				link: '?'
+			});
+			Paint.list('#videos_yt', Videos.youtube);
+		}
+	});
+};
