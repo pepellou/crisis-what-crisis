6575aff3b1d43b9448be54b85821c181f1986875
Pepe Doval <pepe.doval@getsetpay.com>
2014-11-23 07:52:34 +0100

    Publish/unpublish youtube videos

--- olakease/admin.js
+++ olakease/admin.js
@@ -16,6 +16,19 @@ var YT_VIDEO_ACTIONS = [
 	{ name: 'publish', message: 'Publicar' }
 ];
 
+var handleAction = function(action) {
+	var action = $(action.target);
+	var id = action.data('video');
+	switch (action.attr('name')) {
+		case 'publish':
+			videos.push(Videos.youtube[id].data());
+			break;
+		case 'unpublish':
+			videos.child(Videos.web[id].key).remove();
+			break;
+	}
+};
+
 var Paint = {
 	list: function(where, list) {
 		$(where).html('');
@@ -23,18 +36,28 @@ var Paint = {
 			var item = list[id];
 			$(where).append(ich.item_tpl(item));
 		};
+		$(where + " a").click(handleAction);
 	}
 };
 
 $(function() {
 	videos.on("child_added", function(child) {
-		var video = new Video({ web: child.val() });
+		var video = new Video({ web: child });
 		Videos.web[video.id] = video;
 		Paint.list('#videos_web', Videos.web);
 		if (Videos.youtube[video.id]) {
 			delete Videos.youtube[video.id];
 			Paint.list('#videos_yt', Videos.youtube);
 		}
+	}, errors);
+	videos.on("child_removed", function(child) {
+		var video = new Video({ web: child });
+		delete Videos.web[video.id];
+		Paint.list('#videos_web', Videos.web);
+		delete video.key;
+		video.actions = YT_VIDEO_ACTIONS;
+		Videos.youtube[video.id] = video;
+		Paint.list('#videos_yt', Videos.youtube);
 	}, errors);
 });
 
@@ -52,10 +75,28 @@ function Video(options) {
 		return self;
 	};
 
-	self._createFromWeb = function(data) {
-		for (var field in data) {
-			self[field] = data[field];
+	self.isEligible = function() {
+		var firstVideoDate = '2014-09-14';
+		return self.published >= firstVideoDate
+			|| self.title.substr(0, 3) == 'CWC';
+	};
+
+	self.data = function() {
+		var data = {};
+		for (var field in self) {
+			if (typeof(self[field]) != "function") {
+				data[field] = self[field];
+			}
 		}
+		return data;
+	};
+
+	self._createFromWeb = function(data) {
+		var fields = data.val();
+		for (var field in fields) {
+			self[field] = fields[field];
+		}
+		self.key = data.key();
 		self.actions = WEB_VIDEO_ACTIONS;
 	};
 
@@ -86,12 +127,6 @@ function Video(options) {
 		}
 
 		self.actions = YT_VIDEO_ACTIONS;
-	};
-
-	self.isEligible = function() {
-		var firstVideoDate = '2014-09-14';
-		return self.published >= firstVideoDate
-			|| self.title.substr(0, 3) == 'CWC';
 	};
 
 	return self.init();
